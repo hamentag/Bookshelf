05f9dc5cab81292202d57edb9a0ca311200538ea
Hamza Amentag <hamentag1@gmail.com>
2023-12-18 00:28:55 -0600

    Added sorting bookshelf functionality by title, author name, and language &  updated styles

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Bring data from book-data.js file
 const data = bookData;
-console.log(data)
 // References
 const header = document.getElementById('header');
 const bookshelfEl = document.getElementById('bookshelf');
+
 // Classes declaration
 class Book {
     constructor(_author, _language, _subject, _title){
@@ -15,26 +15,17 @@ class Book {
     render(){
         const bookLi = document.createElement('li');
         bookLi.classList.add('book');
-
         bookLi.innerHTML = `
-            <table>
-                <tr>
-                    <td>Author</td>
-                    <td>${this.author.join(' / ')}</td>
-                </tr>
-                <tr>
-                    <td>Language</td>
-                    <td>${this.language.join(' / ')}</td>
-                </tr>
-                <tr>
-                    <td>Subject</td>
-                    <td>${this.subject.join(' / ')}</td>
-                </tr>
-                <tr>
-                    <td>Title</td>
-                    <td>${this.title.join(' / ')}</td>
-                </tr>
-            </table>
+            <div class="tip">
+                <table>
+                    <tr><td>Author</td><td>${this.author.join(' / ')}</td></tr>
+                    <tr><td>Title</td><td>${this.title.join(' / ')}</td></tr>
+                    <tr><td>Language</td><td>${this.language.join(' / ')}</td></tr>                  
+                </table>
+                <div class="more">
+                    Subject: ${this.subject.join(' / ')}
+                </div>
+            </div>
             `;
         return bookLi;
     }
@@ -49,16 +40,31 @@ class Bookshelf{
     }
     render(){
         const bookshelfUl = document.createElement('ul');
-        // bookshelfUl.classList.add('bookshelf');
         this.books.forEach(book => {
             bookshelfUl.appendChild(book.render());
         });
         return bookshelfUl;
     }
+
+    sort(key){
+        const sortedList = [...this.books].sort(function(a, b) {
+            const nameA = a[`${key}`][0].toUpperCase();
+            const nameB = b[`${key}`][0].toUpperCase();
+            if (nameA < nameB) {
+              return -1;
+            }
+            if (nameA > nameB) {
+              return 1;
+            }
+            return 0;
+        });
+        return sortedList;
+    }
 }
 
 // create instances
 const bookshelf = new Bookshelf();
+
 // data.forEach(book => {
 //     bookshelf.addBook(Object.setPrototypeOf(book, Book.prototype))
 // });
@@ -67,4 +73,37 @@ data.forEach(book => {
     bookshelf.addBook(new Book(book.author, book.language, book.subject, book.title));
 });
 
-bookshelfEl.replaceChildren(bookshelf.render());
+// Add event listeners to checkboxes
+const sortCheckboxes = document.querySelectorAll('input[type="checkbox"]');
+sortCheckboxes.forEach(function(checkbox) {
+    checkbox.addEventListener('change', function() {
+        if (this.checked) {
+            // All other checkboxes are turned unchecked
+            sortCheckboxes.forEach(function(otherCheckbox) {
+                if (otherCheckbox !== checkbox) {
+                    otherCheckbox.checked = false;
+                }
+        });
+
+        //  create Bookshelf instance for the sorted list
+        const sortedLst = new Bookshelf();
+        // sort the list by the slected key and create Book instances to fill the sorted list
+        bookshelf.sort(this.name.slice(8)).forEach(book => {
+            sortedLst.addBook(Object.setPrototypeOf(book, Book.prototype))
+        });
+        // display the sorted list
+        bookshelfEl.replaceChildren(sortedLst.render());
+        }
+        else{
+            // otherwise display the original list
+            init();
+        }
+    });
+});
+
+// initial state: Display the original list
+function init(){
+    bookshelfEl.replaceChildren(bookshelf.render());
+}
+
+init();
